6c8e8503b109f865cc405101b5097a748a7c3e07
Mahmud Hossain Sushmoy <sushmoy@duck.com>
2026-06-10 13:27:37 +0600

    migration reset

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+export const db = drizzle(process.env.DATABASE_URL!, { schema, logger: true });
 
 // if (process.env.NODE_ENV !== "production") {
 // // eslint-disable-next-line
